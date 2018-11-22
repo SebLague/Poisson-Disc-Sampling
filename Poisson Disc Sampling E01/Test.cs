b3776c7adb9b4561294798405fc361995e0fc895
Sebastian <sebastlague@gmail.com>
2018-11-22 20:42:46 +0100

    Episode 01

---
+++ Poisson Disc Sampling E01/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public float radius = 1;
+	public Vector2 regionSize = Vector2.one;
+	public int rejectionSamples = 30;
+	public float displayRadius =1;
+
+	List<Vector2> points;
+
+	void OnValidate() {
+		points = PoissonDiscSampling.GeneratePoints(radius, regionSize, rejectionSamples);
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(regionSize/2,regionSize);
+		if (points != null) {
+			foreach (Vector2 point in points) {
+				Gizmos.DrawSphere(point, displayRadius);
+			}
+		}
+	}
+}
